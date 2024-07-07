f3e26fb8ac5e60c6ea49aa07cee3a1ae245a5e5d
Aamir-Yousaf <aamir.yousaf.dev@gmail.com>
2024-07-07 23:36:20 +0500

    add loader

--- src/Albums/Albums.js
+++ src/Albums/Albums.js
@@ -1,31 +1,60 @@
 import { useEffect, useState } from "react";
+import { Spinner, Toast, ToastHeader, Card, CardBody, CardHeader } from "reactstrap";
 
 export default function Albums() {
   const [albumsList, setAlbumsList] = useState([]);
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
 
-  // Fetch data from API or database
   useEffect(() => {
+    // Fetch data from API or database
     fetch("https://jsonplaceholder.typicode.com/albums")
       .then((res) => res.json())
       .then((data) => {
         setAlbumsList(data);
         setError("");
+        setLoading(false);
       })
-      .catch((err) => setError(err.message)); 
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
   }, []);
 
   return (
     <div>
-      {albumsList.map((item, index) => (
-        <div key={index}>
-          <h3>
-            {index + 1}).{item.title}
-          </h3>
+      {loading ? (
+        <div>
+          <Spinner color="primary" size="sm">
+            Loading...
+          </Spinner>
+          <Spinner color="primary" size="sm" type="grow">
+            Loading...
+          </Spinner>
         </div>
+      ) : (
+        ""
+      )}
+      {albumsList.map((item, index) => (
+        <Card
+          style={{
+            width: "18rem",
+          }}
+        >
+          <CardBody>
+            <CardHeader>
+              <span className="text-primary">{index + 1}).</span> {item.title}
+            </CardHeader>
+          </CardBody>
+        </Card>
       ))}
-      <p>{error}</p>
+      {error ? (
+        <div className="p-3 bg-danger my-2 rounded">
+          <Toast>
+            <ToastHeader>{error}</ToastHeader>
+          </Toast>{" "}
+        </div>
+      ) : null}
     </div>
   );
 }
- 
